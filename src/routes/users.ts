8a1df9e37703d94ebd586af1d5ddd931f34916e6
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-11-02 12:56:18 -0500

    import function in routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import passportGoogle from '../strategy/passport-google-oauth20';
 import passportJWT from '../strategy/passport-jwt';
-import { loginOrRegister } from '../controllers/users';
+import { loginOrRegister, refresh } from '../controllers/users';
 
 const router = express.Router();
 
 router.get('/google', passportGoogle.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', passportGoogle.authenticate('google', { session: false }), loginOrRegister);
-// router.get('/refresh', refresh);
+router.get('/refresh', refresh);
 router.get('/protected', passportJWT.authenticate('jwt', { session: false }), (req, res) => {
     res.status(200).json({
         msg: 'You are authorized',
