e9f4cbea4ccfcc7147fb8a1ac2f651101fbc7189
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-11-02 11:28:53 -0500

    refactor user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,18 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
+export interface IUser extends mongoose.Document {
+    email: string;
+    password: {
+        salt: string;
+        hash: string;
+    };
+}
 
-const UserSchema = new Schema({
-    email: String,
-    hash: String,
-    salt: String,
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: {
+        salt: { type: String, required: true },
+        hash: { type: String, required: true },
+    },
 });
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model<IUser>('User', UserSchema);
